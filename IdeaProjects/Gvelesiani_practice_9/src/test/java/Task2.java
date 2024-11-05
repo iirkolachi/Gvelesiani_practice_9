1a88e879e56f641b207e8d8f37d28d653d37f7fa
Tinatin Gvelesiani <tinatingvelesiani@credo.ge>
2024-11-05 20:52:19 +0400

    task_2_Tinatin

--- IdeaProjects/Gvelesiani_practice_9/src/test/java/Task2.java
+++ IdeaProjects/Gvelesiani_practice_9/src/test/java/Task2.java
@@ -2,18 +2,20 @@ import java.util.HashSet;
 
 public class Task2 {
     public static void main(String[] args) {
-        HashSet<Integer> numbers = new HashSet<>();
-        numbers.add(10);
-        numbers.add(20);
-        numbers.add(30);
-        numbers.add(20);
-        numbers.add(10);
-        System.out.println(numbers);
+        HashSet<Integer> ricxvebi = new HashSet<>();
+        ricxvebi.add(5);
+        ricxvebi.add(7);
+        ricxvebi.add(7);
+        ricxvebi.add(5);
+        ricxvebi.add(3);
+        System.out.println(ricxvebi);
 
-        if (numbers.contains(20)) {
-            System.out.println("Set contains 20");
+        if (ricxvebi.contains(9)) {
+            System.out.println("Set contains 9.");
+        } else {
+            System.out.println("Set does not contain 9.");
         }
-        for (Integer num : numbers) {
+        for (Integer num : ricxvebi) {
             System.out.println(num);
         }
     }
