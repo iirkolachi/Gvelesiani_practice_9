753924beafb51f7ac5702444e3f5f01c80856f7f
Tinatin Gvelesiani <tinatingvelesiani@credo.ge>
2024-11-05 20:48:46 +0400

    task_1_Tinatin

--- IdeaProjects/Gvelesiani_practice_9/src/test/java/Task1.java
+++ IdeaProjects/Gvelesiani_practice_9/src/test/java/Task1.java
@@ -2,13 +2,13 @@ import java.util.ArrayList;
 
 public class Task1 {
     public static void main(String[] args) {
-        ArrayList<String> colors = new ArrayList<>();
-        colors.add("Red");
-        colors.add("Green");
-        colors.add("Blue");
-        System.out.println(colors);
-        System.out.println("Second color: " + colors.get(1));
-        colors.remove(0);
-        System.out.println("After removal: " + colors);
+        ArrayList<String> ferebi = new ArrayList<>();
+        ferebi.add("წითელი");
+        ferebi.add("ყვითელი");
+        ferebi.add("მწვანე");
+        System.out.println(ferebi);
+        System.out.println("მეორე ფერია: " + ferebi.get(1));
+        ferebi.remove(2);
+        System.out.println("დარჩა: " + ferebi);
     }
 }
