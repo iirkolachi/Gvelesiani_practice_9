1dcf59c88527e307bef561235a08b01c6e30ce65
Tinatin Gvelesiani <tinatingvelesiani@credo.ge>
2024-11-06 23:37:12 +0400

    task_1_Tinatin

---
+++ IdeaProjects/Gvelesiani_practice_10/src/test/java/Task1.java
@@ -0,0 +1,12 @@
+import java.util.Arrays;
+import java.util.List;
+
+public class Task1 {
+    public static void main(String[] args) {
+        List<String> names = Arrays.asList("Alice", "Bob", "Eve", "Charlie", "Daniel");
+
+        List<String> namesFiltered = names.stream().filter(item -> item.contains("e")).toList();
+
+        namesFiltered.forEach(System.out::println);
+    }
+}
