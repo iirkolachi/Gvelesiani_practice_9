ccc02a3dfc571c3388de8149a8b7fdc1bde78370
Tinatin Gvelesiani <tinatingvelesiani@credo.ge>
2024-11-06 23:47:15 +0400

    task_1_Tinatin

--- IdeaProjects/Gvelesiani_practice_10/src/test/java/Task1.java
+++ IdeaProjects/Gvelesiani_practice_10/src/test/java/Task1.java
@@ -5,8 +5,12 @@ public class Task1 {
     public static void main(String[] args) {
         List<String> names = Arrays.asList("Alice", "Bob", "Eve", "Charlie", "Daniel");
 
-        List<String> namesFiltered = names.stream().filter(item -> item.contains("e")).toList();
+        List<String> namesFiltered = names.stream()
+                                    .filter(item -> item.contains("e"))
+                                    .toList(); /*collect(Collectors.toList());
+                                                 ინტელიჯეიმ შემომთავაზა ოპტიმიზაცია*/
 
-        namesFiltered.forEach(System.out::println);
+        namesFiltered.forEach(System.out::println); /*(item -> System.out.println(item));
+                                                               ინტელიჯეიმ შემომთავაზა ოპტიმიზაცია*/
     }
 }
